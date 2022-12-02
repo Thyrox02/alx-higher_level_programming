af9f1cc3d13e522f0e2e3645a35af4029189c235
lukman <lukmanafolayan@gmail.com>
2022-12-02 05:01:32 +0000

    asgr

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     if i >= 1:
         i = 0
         for arg in sys.argv:
-            if i != 0:
-                print("{}: {}".format(i, arg))
-            i += 1
+            
+     if i != 0:
+        print("{}: {}".format(i, arg))
+          i += 1
